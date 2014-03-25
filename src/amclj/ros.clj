6484e58da8f6114cd5e76df7e8ff671a83c91978
Jeremiah Via <jeremiah.via@gmail.com>
2014-03-25 18:34:11 -0400

    Can now publish messages.

--- src/amclj/ros.clj
+++ src/amclj/ros.clj
@@ -15,12 +15,9 @@
          (getData [] @data)
          (setData [newdata] (reset! data newdata))))))
 
-(defprotocol IPublish
-  (publish [_ topic type msg]))
-
-(defprotocol ISubscribe
-  (subscribe [_ topic type callback-fn ]))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Node creation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defprotocol INode
   (start [_] "Start the node")
   (stop [_] "Stop the node")
@@ -28,18 +25,12 @@
 
 (defrecord RosNode
     [name connection configuration
-     executor nodemain publishers subscribers]
-  INode
-  (start [node]
-        (let [executor (:executor node)
-              nodemain (:nodemain node)
-              configuration (:configuration node)]
-      (. executor execute  nodemain configuration)))
-  (stop [node]
-    (let [executor (:executor node)
-          nodemain (:nodemain node)]
-      (. executor shutdownNodeMain nodemain)))
-  (running? [node] (-> node :connection deref nil? not)))
+     executor nodemain publishers subscribers])
+
+(defn- connection
+  "Get the connection of a node"
+  [node]
+  @(:connection node))
 
 (defn rosnode [name]
   (let [conn (atom nil)
@@ -52,3 +43,70 @@
         rosnode (->RosNode name conn conf exec main {} {})]
     rosnode))
 
+(extend-type RosNode
+  INode
+  (start [node]
+    (let [executor (:executor node)
+          nodemain (:nodemain node)
+          configuration (:configuration node)]
+      (. executor execute  nodemain configuration)
+      node))
+  (stop [node]
+    (let [executor (:executor node)
+          nodemain (:nodemain node)]
+      (. executor shutdownNodeMain nodemain))
+    node)
+  (running? [node] (-> node :connection deref nil? not)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Node communication
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol IPublish
+  (publish [node topic msg] "Publish a message")
+  (add-publisher [node topic type] "Add a new publisher.")
+  (new-message [node topic] "Creates a new message to publish on the given topic"))
+
+
+(defn- add-publisher* [node topic typestr]
+  (when-not (running? node)
+    (throw (IllegalStateException. (str "Node " (:name node) " is not running"))))
+  (let [connection (connection node)
+        publisher (.newPublisher connection topic typestr)]
+    (update-in node [:publishers] assoc topic publisher)))
+
+;; (defn- restr
+;;   "Perform a string version of rest"
+;;   [string]
+;;   (apply str (rest string)))
+
+;; (defn- javaize [name]
+;;   (let [m (-> name str restr)]
+;;     (str ".set" (str/capitalize m))))
+
+;; (defmacro rosmsg [publisher data]
+;;     `(doto (.newMessage ~publisher)
+;;        ~@(for [[m v] (zipmap (map (comp symbol javaize key) data)
+;;                              (map val data))]
+;;            (list m v))))
+
+(defn new-message* [node topic]
+  (if-let [publisher (get-in node [:publishers topic])]
+    (.newMessage publisher)
+    (throw (IllegalArgumentException. (str "No publisher for " topic)))))
+
+(defn- publish* [node topic msg]
+  (if-let [publisher (get-in node [:publishers topic])]
+    (.publish publisher msg)
+    (throw (IllegalArgumentException. (str "No publisher for " topic)))))
+
+
+(extend-type RosNode
+  IPublish
+  (publish [node topic msg] (publish* node topic msg))
+  (add-publisher [node topic type] (add-publisher* node topic type))
+  (new-message [node topic] (new-message* node topic)))
+
+
+(defprotocol ISubscribe
+  (subscribe [_ topic type callback-fn]))
