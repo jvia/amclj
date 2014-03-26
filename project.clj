b57477e435df62e0fad75fc564276a344ad39a72
Jeremiah Via <jeremiah.via@gmail.com>
2014-03-26 16:55:23 -0400

    Use locally installed rosjava jars.

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.ros.rosjava_core/rosjava "0.1.6"]
+                 ;;[org.ros.rosjava_core/rosjava "0.1.6"]
+                 [ros.rosjava_core/rosjava_bootstrap "0.0.0-SNAPSHOT"]
+                 [ros.rosjava_core/rosjava "0.0.0-SNAPSHOT"]
+                 [ros.rosjava_core/rosjava_messages "0.0.0-SNAPSHOT"]
                  [midje "1.6.2"]]
   :repositories [["rosjava" "https://github.com/rosjava/rosjava_mvn_repo/raw/master"]])
